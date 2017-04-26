cb67d34d778292303dcad0144c597ed189258bab
schiavonjp <schiavonjp@yahoo.com.br>
2017-04-25 23:43:51 -0300

    Caracteres invalidos ++

--- main.rb
+++ main.rb
@@ -8,17 +8,18 @@ class Lexi
   reserved = ["const","div","or","and","not","if","then","else","of","while","do","begin","end","read","write","var","array","function","procedure","program","true","false","char","integer","boolean"]
   lexema = String.new
   puts "Entre com o texto a ser analisada:"
-  input = "// lalal
-if 13 ai "
+  input = "program programa1;
+begin
+@aux=10.65;
+end. "
   state = 0
-  puts "#{input.split("\n")}"
-  for line in input.split("\n")
-    state = 0
-    for character in line.chars                #Percorre o input todo
-      #puts "#{character}, #{state},#{lexema}"
+  line = 0
+  puts "#{input.split("")}"
+    for character in input.split("")              #Percorre o input todo
+      puts "#{character}, #{state},#{lexema}"
+      #puts "linha #{line+1}"
       case state   # Verifica em que estado o automato está
         when 0
-
           if letters.include? character
             lexema << character
             state = 1
@@ -28,21 +29,39 @@ if 13 ai "
           elsif character == "/"
             lexema << character
             state = 5
+          elsif special.include? character
+            lexema << character
+            state = 6
+          else
+            puts "Caracter inválido #{character}"
           end
         when 1
-          if (letters.include? character or numbers.include? character)
+          if special.include? character
+            puts "Palavra Reservada #{lexema}"
+            lexema = String.new
             lexema << character
-          elsif  reserved.include? lexema
+            state = 6
+          elsif (letters.include? character or numbers.include? character)
+            lexema << character
+          elsif  reserved.include? lexema and !(special.include? character)
             puts "Palavra Reservada #{lexema}"
             lexema = String.new
             state = 0
-          else !(reserved.include? lexema)
+          elsif !(reserved.include? lexema)
             puts "Identificador #{lexema}"
             lexema = String.new
             state = 0
           end
         when 2
-          if numbers.include? character
+          if character == "."
+            lexema << character
+            state =3
+          elsif special.include? character
+            puts "Inteiro válido #{lexema}"
+            lexema = String.new
+            lexema << character
+            state = 6
+          elsif numbers.include? character
             lexema << character
           elsif end_string.include? character and (lexema.to_i).abs <= 4294967295
             puts "Inteiro Válido #{lexema}"
@@ -61,14 +80,19 @@ if 13 ai "
             lexema << character
             state = 4
           else
-            puts "Real inválido"
+            puts "Real inválido #{lexema}"
             state = 0
           end
         when 4
-          if end_string.include? character and lexema.to_f <= 3.4*(10**38)
-            puts "Real Válido"
+          lexema << character
+          if  (lexema.to_f).abs <= (3.4*(10**38))
+            puts "Real Válido #{lexema}"
+            lexema = String.new
+            state = 0
           else
-            puts "OverFlow Real"
+            puts "OverFlow Real #{lexema}"
+            lexema = String.new
+            state = 0
           end
         when 5
           lexema << character
@@ -79,9 +103,17 @@ if 13 ai "
             state = 0
             break
           end
+        when 6
+          if end_string.include? character or special.include? lexema
+            puts "Simbolo Reservado #{lexema}"
+            lexema = String.new
+            lexema << character
+            state = 0
+          else
+            lexema << character
+          end
         else
           puts "Error na entrada"
       end
     end
-  end
 end
