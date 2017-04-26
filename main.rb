d1b9e6f67822d3cf0721692ad221c4d30debe45a
schiavonjp <schiavonjp@yahoo.com.br>
2017-04-25 21:06:42 -0300

    Comment ++

--- main.rb
+++ main.rb
@@ -1,73 +1,87 @@
 class Lexi
   # Palavras do analisador
-  letters = "ABCDEFGHIJKLMNOPQRSTUVXWYZabcdefghijklmnopqrstuvxwyz"
-  numbers = "0123456789"
-  comment = ["{","}","|","//"]
+  letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","X","W","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","x","w","y","z"]
+  numbers = ["0","1","2","3","4","5","6","7","8","9"]
+  comment = ["{","}","|","/"]
   end_string = ["\n"," ",";"]
-  special = ["+","-","*","/",",",".","=","<","<=",">",">=",":",";","[","]",":=","..."]
+  special = ["+","-","*","/",",",".","=","<","<=",">",">=",":",";","[","]",":=","...","<>"]
   reserved = ["const","div","or","and","not","if","then","else","of","while","do","begin","end","read","write","var","array","function","procedure","program","true","false","char","integer","boolean"]
   lexema = String.new
   puts "Entre com o texto a ser analisada:"
-  input = gets
+  input = "// lalal
+if 13 ai "
   state = 0
+  puts "#{input.split("\n")}"
+  for line in input.split("\n")
+    state = 0
+    for character in line.chars                #Percorre o input todo
+      #puts "#{character}, #{state},#{lexema}"
+      case state   # Verifica em que estado o automato está
+        when 0
 
-  for character in input.chars                #Percorre o input todo
-    puts "#{character}, #{state},#{lexema}"
-    case state   # Verifica em que estado o automato está
-      when 0 then
-        if letters.include? character
-          lexema << character
-          state = 1
-        elsif numbers.include? character
-          lexema << character
-          state = 2
-        elsif comment.include? character
-          lexema << character
-          state = 3
-        end
-      when 1 then
-        if (letters.include? character or numbers.include? character) and !(end_string.include? character)
-          lexema << character
-        elsif end_string.include? character
-          if reserved.include? lexema     #Verifica se o lexema esta nas palavras reservadas
-            puts "Palavra reservada"
-          elsif special.include? lexema
-            puts "Simbolos Reservados"
-          else
-            puts "Identificador"
+          if letters.include? character
+            lexema << character
+            state = 1
+          elsif numbers.include? character
+            lexema << character
+            state = 2
+          elsif character == "/"
+            lexema << character
+            state = 5
           end
-        end
-      when 2 then
-        if numbers.include? character and !(end_string.include? character)
-          lexema << character
-        elsif end_string.include? character
-          if (lexama.to_i).abs <= 4294967295             #Verifica se o tamanho do inteiro é compativel com o Pascal
-            puts "Inteiro Válido"
-          else
-            puts "OverFlow Inteiro"
+        when 1
+          if (letters.include? character or numbers.include? character)
+            lexema << character
+          elsif  reserved.include? lexema
+            puts "Palavra Reservada #{lexema}"
+            lexema = String.new
+            state = 0
+          else !(reserved.include? lexema)
+            puts "Identificador #{lexema}"
+            lexema = String.new
+            state = 0
           end
-        elsif character == "."              #Se verdadeiro então se trata de um número real
-          lexema << character
-          state = 3
-        end
-      when 3 then
-        if numbers.include? character and !(end_string.include? character)
-          lexema << character
-          state = 4
-        else
-          "Error: Verifique sua entrada"
-        end
-      when 4 then
-        if end_string.include? character
-          if lexema.to_f <= 3.4*(10**38)
+        when 2
+          if numbers.include? character
+            lexema << character
+          elsif end_string.include? character and (lexema.to_i).abs <= 4294967295
+            puts "Inteiro Válido #{lexema}"
+            lexema = String.new
+            state = 0
+          elsif end_string.include? character
+            puts "OverFlow inteiro"
+            lexema = String.new
+            state = 0
+          elsif character == "."
+            lexema << character
+            state =3
+          end
+        when 3
+          if numbers.include? character
+            lexema << character
+            state = 4
+          else
+            puts "Real inválido"
+            state = 0
+          end
+        when 4
+          if end_string.include? character and lexema.to_f <= 3.4*(10**38)
             puts "Real Válido"
           else
             puts "OverFlow Real"
           end
-        end
-      else
-        printf
+        when 5
+          lexema << character
+          if lexema == "//"
+            puts "comentario #{lexema}"
+          else
+            lexema = String.new
+            state = 0
+            break
+          end
+        else
+          puts "Error na entrada"
+      end
     end
   end
-
 end
